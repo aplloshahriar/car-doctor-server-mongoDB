8e658bc1c97ee841c9134cb2ed9251a2121b70c3
aplloshahriar <aaronshahriar.as@gmail.com>
2023-06-18 23:34:06 +0600

    jwt token worked

--- index.js
+++ index.js
@@ -25,6 +25,28 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const verifyJWT = (req, res, next) => {
+  console.log('hitting verify jwt');
+  console.log(req.headers.authorization);
+  const authorization=req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:'unauthorized access'})
+  }
+  const token=authorization.split(' ')[1];
+  console.log('token inside verify Jwt',token);
+  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN,(error, decoded)=> {
+    if(error){
+      res.status(401).send({error:true,message:'unauthorized access'})
+    }
+    req.decoded=decoded;
+    next();
+  })
+
+}
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -42,8 +64,8 @@ async function run() {
 
       const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '1h' });
       console.log(token);
-      res.send({token});
-      
+      res.send({ token });
+
     })
 
 
@@ -69,9 +91,12 @@ async function run() {
     })
 
 
-    // bookings
-    app.get('/bookings', async (req, res) => {
-      console.log(req.query.email);
+    // bookings routes
+    app.get('/bookings', verifyJWT, async (req, res) => {
+      const decoded=req.decoded;
+      // console.log(req.headers.authorization);
+      console.log('comes after verify',decoded);
+
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email }
