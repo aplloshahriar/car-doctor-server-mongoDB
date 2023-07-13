bc5c7a82c8fa1590ea023ff6d5db333e8703b4d6
aplloshahriar <aaronshahriar.as@gmail.com>
2023-07-13 16:58:03 +0600

    service search button added with backend

--- index.js
+++ index.js
@@ -73,12 +73,17 @@ async function run() {
     // services
     app.get('/services', async (req, res) => {
       const sort=req.query.sort;
-      const query={};
+      const search=req.query.search;
+      // const query={};
+      // const query={price:{$gte:50, $lte:150}};
+      const query={title:{$regex:search,$options:'i'}};
       const options = {
         // sort matched documents in descending order by rating
         sort: { 
           "price" : sort=== 'asc' ? 1 : -1
         },
+
+
         // Include only the `title` and `imdb` fields in the returned document
       
       };
